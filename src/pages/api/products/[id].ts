94e136b695152d15b4da61489e458b91f3f1de29
rades12340 <rades12340@gmail.com>
2020-07-22 02:25:04 +0200

    improvedq

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -1,10 +1,10 @@
 import con from "../../../../connection";
 import { NextApiRequest, NextApiResponse } from "next";
-import { query } from "../../../getAsString";
+import { getAsString } from "../../../getAsString";
 
 export default async function getAllProducts(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const id = query(req.query.id);
+        const id = getAsString(req.query.id);
         con.query('select * from product inner join image on product.product_id = image.product_id where product.product_id = ?', [id], (err, result) => {
             res.send(result)
         })
